ff5e62c4543964c548c0812a80d6f6f9c46335e0
Uriel Cota <ucota_24@alu.uabcs.mx>
2026-02-10 20:54:54 -0700

    Avance 2 (Login)

--- src/main/Ventana.java
+++ src/main/Ventana.java
@@ -10,15 +10,15 @@ import views.MiPanel;
 public class Ventana extends JFrame {
 	
 	public Ventana() {
-		setSize(500,500);
+		setSize(700,700);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocation(100,100);
 		setResizable(false);
-		setTitle("Mi ventana 2.0");
+		setTitle("Tienda de Tenis");
 		setLocationRelativeTo(null);
 		
 		Toolkit tk = Toolkit.getDefaultToolkit();
-		Image myIcon = tk.getImage("src/image/icono.jpg");
+		Image myIcon = tk.getImage("src/image/logoteniventana.jpg");
 		setIconImage(myIcon);
 		
 		MiPanel panelito= new MiPanel();
--- src/views/MiPanel.java
+++ src/views/MiPanel.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Font;
 
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -11,29 +12,54 @@ import javax.swing.JLabel;
 public class MiPanel extends JPanel {
 	
 	public MiPanel() {
-		setBackground(Color.WHITE);
+		setBackground(new Color(245, 245, 220));
 		setLayout(null);
 		
-		JButton boton = new JButton("Mi boton");
-		add(boton);
-		
-		JLabel label = new JLabel ("Inicia Sesion");
-		label.setFont(new Font("Arial" , Font.PLAIN,20));
-		label.setBounds(180,40,500,100);
-		add(label);
-		
-		JTextField textField = new JTextField();
-		textField.setFont(new Font("Arial" , Font.PLAIN,30));
-		textField.setBounds(10,100,200,50);
-		add(textField);
-		
-		
-		
-		//setBackground(new Color(210, 165, 35));
-		
+		tituloPagina();
+		boton();
+		ingresarCorreo();
+		ingresarContrasena();
+				
 	}
 	
+	public void tituloPagina () {
+		JLabel label = new JLabel ("Iniciar Sesion");
+		label.setFont(new Font("Arial" , Font.PLAIN,20));
+		label.setBounds(280,5,500,100);
+		add(label);
+	}
 	
+	public void boton() {
+		JButton boton = new JButton("INGRESAR");
+		boton.setBounds(265,555,150,50);
+		//boton.setBackground(Color.lightGray);
+		
+		add(boton);
+	}
+	
+	public void ingresarCorreo() {
+		JLabel txtnombre = new JLabel ("Ingrese su correo electronico");
+		txtnombre.setFont(new Font("Arial" , Font.PLAIN,20));
+		txtnombre.setBounds(205,170,500,100);
+		add(txtnombre);
+		
+		JTextField textField = new JTextField();
+		textField.setFont(new Font("Arial" , Font.PLAIN,18));
+		textField.setBounds(205,245,260,35);
+		add(textField);
+	}
+	
+	public void ingresarContrasena() {
+		JLabel txtcontrasena = new JLabel ("Ingrese su contraseña");
+		txtcontrasena.setFont(new Font("Arial", Font.PLAIN,20));
+		txtcontrasena.setBounds(230,300,260,50);
+		add(txtcontrasena);
+		
+		JPasswordField contrasena = new JPasswordField();
+		contrasena.setFont(new Font("Arial", Font.PLAIN,18));
+		contrasena.setBounds(205,350,260,35);
+		add(contrasena);
+	}
 	
 	
 
